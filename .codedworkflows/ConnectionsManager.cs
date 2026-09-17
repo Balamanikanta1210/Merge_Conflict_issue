8da754ee083f03742afd2a3ae63b60dfccb1747a
Balamanikanta K <balamanikanta.krishnam@uipath.com>
2026-09-17 16:25:58 +0530

    dadada

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace Merge_ConflictIssue
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
